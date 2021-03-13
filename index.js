6a5d6702995ccb89ca8ea2a121c508ce310213cc
Jakub Muszynski <71202201+Jakub-Muszynski@users.noreply.github.com>
2021-03-13 23:52:22 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const c = document.createElement("canvas");
 const ctx = c.getContext("2d");
-c.width = 900;
-c.height = 900;
+c.width = 800;
+c.height = 800;
 document.body.appendChild(c);
 
 let perm = [];
