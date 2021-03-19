d86d335725abfe9dc6d6ce5c5eb86021146aa30c
Jakub Muszynski <71202201+Jakub-Muszynski@users.noreply.github.com>
2021-03-19 22:14:07 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const c = document.createElement("canvas");
 const ctx = c.getContext("2d");
-c.width = 900;
-c.height = 900;
+c.width = 1000;
+c.height = 1000;
 document.body.appendChild(c);
 
 let perm = [];
